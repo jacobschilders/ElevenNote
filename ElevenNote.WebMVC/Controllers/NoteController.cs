e6fdfd12ef978cded40b8d12122cc06872d7a5cf
jacobschilders <jschilders@anderson.edu>
2020-11-11 10:01:42 -0500

    Create Model

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -17,5 +17,23 @@ namespace ElevenNote.WebMVC.Controllers
             var model = new NoteListItem[0];
             return View(model);
         }
+
+        //Get: NoteCreate
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //Post: NoteCreate
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
+        }
     }
 }
